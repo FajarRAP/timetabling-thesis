61cb9b1d204ccbf85f07b55ac8067ecc1c823e51
FajarRiansyah <fajary781@gmail.com>
2025-03-15 18:56:04 +0700

    feat: call all seeder

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call([
+            DaySeeder::class,
+            RoomClassSeeder::class,
+            CourseSeeder::class,
+            LecturerSeeder::class,
+            LectureSeeder::class,
+            TimeSlotSeeder::class,
+            LectureSlotSeeder::class,
+        ]);
+    }
+}
