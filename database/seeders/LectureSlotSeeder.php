c0927025e7138dac404252b31502c106926c4506
Fajar Riansyah <fajary781@gmail.com>
2025-03-19 14:46:51 +0700

    feat (LectureSlotSeeder): make iteration count limit to each table count

--- database/seeders/LectureSlotSeeder.php
+++ database/seeders/LectureSlotSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Day;
+use App\Models\RoomClass;
+use App\Models\TimeSlot;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,11 +16,14 @@ class LectureSlotSeeder extends Seeder
      */
     public function run(): void
     {
+        $dayCount = Day::count();
+        $roomClassCount = RoomClass::count();
+        $timeSlotCount = TimeSlot::count();
         $datas = [];
 
-        for ($i = 0; $i < 6; $i++) { // Active Days (days)
-            for ($j = 0; $j < 7; $j++) { // Total Available Class (room_classes)
-                for ($k = 0; $k < 10; $k++) { // Time Slot (time_slots)
+        for ($i = 0; $i < $dayCount; $i++) { // Active Days (days)
+            for ($j = 0; $j < $roomClassCount; $j++) { // Total Available Class (room_classes)
+                for ($k = 0; $k < $timeSlotCount; $k++) { // Time Slot (time_slots)
                     $datas[] = [
                         'day_id' => $i + 1,
                         'time_slot_id' => $k + 1,
