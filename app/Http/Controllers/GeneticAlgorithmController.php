53fa4a6af3ec89d4265e6004b45c33c0eaf0d471
Fajar Riansyah <fajary781@gmail.com>
2025-03-29 23:51:11 +0700

    feat: add elitism selection method

--- app/Http/Controllers/GeneticAlgorithmController.php
+++ app/Http/Controllers/GeneticAlgorithmController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\LectureResource;
 use App\Http\Resources\LectureSlotResource;
 use App\Models\Lecture;
 use App\Models\LectureSlot;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Collection;
@@ -14,80 +15,73 @@ class GeneticAlgorithmController extends Controller
 {
     public function generate()
     {
-        // Dosen yang bentrok:
-        // - waktu yang sama dengan ruangan yang sama
+        /**
+         * PR BROWW:
+         * - konfliknya diitung 2x, padahal sama hanya tertukar posisi index saja, e.g.: ada di excel -> sementara dibagi 2 dulu, awoakwoak
+         *
+         * Problems Solved:
+         * - Jadi ainx tanggal 17 Maret 2025 mengalami masalah ketika mau bikin timetablenya secara semi deterministik, ternyata masalahnya itu terletak di lecture slot. Pastikan lecture slotnya itu tersedia, contoh kalo misalnya total dari course yang 3 sks itu ada 200 (($course->credit_hour == 3)->count()), maka lecture slot untuk 3 sks harus lebih dari itu (antara ruangan/time slot yang diperbanyak, karena hari ga mungkin).
+         */
 
-        // PR BROWW:
-        // - konfliknya diitung 2x, padahal sama hanya tertukar posisi index saja, e.g.: ada di excel
-
-        $populationSize = 1;
+        $populationSize = 3;
         $population = $this->initializePopulation($populationSize, true);
-        $collections = [];
-        $filteredCollections = [];
+
+        $filteredPopulation = collect([]);
+        // $conflicts = [];
         $conflictCounts = [];
+
+        $mapChromosome = fn(array $value, int $key) =>
+        [
+            'id' => $key,
+            'lecture' => [
+                'course_name' => $value['lecture']->course->course_name,
+                'class' => $value['lecture']->class,
+                'lecturer_name' => $value['lecture']->lecturer->lecturer_name,
+            ],
+            'lecture_slot' => [
+                'lecture_slot_id' => $value['lecture_slot']->id,
+                'day' => $value['lecture_slot']->day->day,
+                'time_slot' => [
+                    'start_at' => $value['lecture_slot']->timeSlot->start_at,
+                    'end_at' => $value['lecture_slot']->timeSlot->end_at
+                ],
+                'room_class' => $value['lecture_slot']->roomClass->room_class,
+            ],
+        ];
 
         for ($i = 0; $i < $populationSize; $i++) {
             $conflictCounts[] = 0;
-            $collections[] = collect($population[$i]);
-
-            $collections[$i] = $collections[$i]->filter(
-                fn(array $value, int $key) =>
-                $value['lecture']['course']['credit_hour'] == 3
-            )->values();
-
-            $filteredCollections[] = $collections[$i]->map(
-                fn(array $value, int $key) =>
-                [
-                    'id' => $key,
-                    'lecture' => [
-                        'course_name' => $value['lecture']->course->course_name,
-                        'class' => $value['lecture']->class,
-                        'lecturer_name' => $value['lecture']->lecturer->lecturer_name,
-                    ],
-                    'lecture_slot' => [
-                        'day' => $value['lecture_slot']->day->day,
-                        'time_slot' => $value['lecture_slot']->timeSlot->time_slot,
-                        'room_class' => $value['lecture_slot']->roomClass->room_class,
-                    ],
-                ]
-            );
-
-            foreach ($filteredCollections[$i] as $key => $value) {
-                foreach ($filteredCollections[$i] as $k => $v) {
-                    // $isSameLecturer = $value['lecture']['lecturer_name'] == $v['lecture']['lecturer_name'];
-                    $isSameRoomClass = $value['lecture_slot']['room_class'] == $v['lecture_slot']['room_class'];
-                    $isSameTimeSlot = $value['lecture_slot']['time_slot'] == $v['lecture_slot']['time_slot'];
-                    $isSameDay = $value['lecture_slot']['day'] == $v['lecture_slot']['day'];
-                    $isNotSameLecture = $key != $k;
-
-                    if ($isSameRoomClass && $isSameTimeSlot && $isSameDay && $isNotSameLecture) {
-                        $conflictCounts[$i]++;
-                    }
-                }
-            }
+            // $filteredPopulation[$i] = $population[$i]->filter(
+            //     fn(array $value, int $key) =>
+            //     $value['lecture']['course']['credit_hour'] == 2
+            // )->values();
+            $filteredPopulation[] = $population[$i]->map($mapChromosome);
+            $chromosomeLength = count($filteredPopulation[$i]);
+            $conflictCounts[$i] = $this->evaluateChromosome($filteredPopulation[$i]);
+            $filteredPopulation[$i][] = ['fitness_score' => $chromosomeLength / ($chromosomeLength + $conflictCounts[$i])];
         }
 
-        $data = [];
+        $bestChromosome = $this->chromosomeSelection($filteredPopulation);
 
-        for ($i = 0; $i < $populationSize; $i++) {
-            $cromosomeLength = count($filteredCollections[$i]);
-            $data[] = [
-                'chromosome_length' => $cromosomeLength,
-                'conflict_count' => $conflictCounts[$i],
-                'fitness_score' => $cromosomeLength / ($cromosomeLength + $conflictCounts[$i]),
-                'conflict_count_normalized' => $conflictCounts[$i] / 2,
-                'fitness_score_normalized' => $cromosomeLength / ($cromosomeLength + $conflictCounts[$i] / 2),
-            ];
-        }
+        // $formattedData = collect([]);
+
+        // for ($i = 0; $i < $populationSize; $i++) {
+        //     $cromosomeLength = count($filteredPopulation[$i]);
+        //     $formattedData->push([
+        //         'chromosome_length' => $cromosomeLength,
+        //         'conflict_count' => $conflictCounts[$i],
+        //         'fitness_score' => $cromosomeLength / ($cromosomeLength + $conflictCounts[$i]),
+        //     ]);
+        // }
+
         return response()->json([
             'message' => 'Successful',
-            'population_size' => count($population),
-            'population' => $data,
-            'chromosome' => $filteredCollections[0],
+            'population_size' => $populationSize,
+            'population' => $filteredPopulation,
         ]);
     }
 
-    private function initializeChromosome(bool $withRandomizeNumber = false): array
+    private function initializeChromosome(bool $modelResource = false)
     {
         $lectures =  LectureResource::collection(Lecture::all());
         [$twoCreditLectureSlots, $threeCreditLectureSlots] = $this->splitLectureSlotsByCreditHour();
@@ -95,27 +89,74 @@ class GeneticAlgorithmController extends Controller
         $timetable = [];
 
         for ($i = 0; $i < $lecturesCount; $i++) {
-            if ($lectures[$i]->course->credit_hour === 2) {
-                $lectureSlot = $twoCreditLectureSlots->random();
-                $timetable[] = $withRandomizeNumber ? ['lecture' => new LectureResource(Lecture::find($i + 1)), 'lecture_slot' => new LectureSlotResource($lectureSlot)] : ['lecture_id' => $i + 1, 'lecture_slot_id' => $lectureSlot->id];
-            } else {
-                $lectureSlot = $threeCreditLectureSlots->random();
-                $timetable[] = $withRandomizeNumber ? ['lecture' => new LectureResource(Lecture::find($i + 1)), 'lecture_slot' => new LectureSlotResource($lectureSlot)] : ['lecture_id' => $i + 1, 'lecture_slot_id' => $lectureSlot->id];
-            }
+            $data = fn(LectureSlotResource $slot) => $modelResource ? [
+                'lecture' => new LectureResource($lectures[$i]),
+                'lecture_slot' => new LectureSlotResource($slot),
+            ] : [
+                'lecture_id' => $lectures[$i]->id,
+                'lecture_slot_id' => $slot->id,
+            ];
+            $isTwoCredit = $lectures[$i]->course->credit_hour === 2;
+
+            /// Delete when added
+            // $timetable[] = $isTwoCredit ?
+            //     $data($twoCreditLectureSlots->pull($twoCreditLectureSlots->keys()->random())) :
+            //     $data($threeCreditLectureSlots->pull($threeCreditLectureSlots->keys()->random()));
+
+            /// Randomized lecture slot without delete
+            $timetable[] = $isTwoCredit ?
+                $data($twoCreditLectureSlots->random()) :
+                $data($threeCreditLectureSlots->random());
         }
 
-        return $timetable;
+        return collect($timetable);
     }
 
-    private function initializePopulation(int $populationSize, bool $withRandomizeNumber = false): array
+    private function initializePopulation(int $populationSize, bool $modelResource = false): array
     {
         $population = [];
 
         for ($i = 0; $i < $populationSize; $i++) {
-            $population[] = $this->initializeChromosome($withRandomizeNumber);
+            $population[] = $this->initializeChromosome($modelResource);
         }
 
         return $population;
+    }
+
+    private function evaluateChromosome(Collection $population)
+    {
+        $conflictCount = 0;
+
+        foreach ($population as $key => $value) {
+            $start_at_first = Carbon::parse($value['lecture_slot']['time_slot']['start_at']);
+            $end_at_first = Carbon::parse($value['lecture_slot']['time_slot']['end_at']);
+
+            foreach ($population as $k => $v) {
+                $start_at_second = Carbon::parse($v['lecture_slot']['time_slot']['start_at']);
+                $end_at_second = Carbon::parse($v['lecture_slot']['time_slot']['end_at']);
+                $isSameRoomClass = $value['lecture_slot']['room_class'] == $v['lecture_slot']['room_class'];
+                $isSameDay = $value['lecture_slot']['day'] == $v['lecture_slot']['day'];
+                $isSameLecture = $key == $k;
+
+                if ($isSameLecture) continue;
+
+                if ($isSameRoomClass && $isSameDay) {
+                    if ($start_at_first < $end_at_second && $end_at_first > $start_at_second) {
+                        $conflictCount++;
+                    }
+                }
+            }
+        }
+
+        return $conflictCount / 2;
+    }
+
+    private function chromosomeSelection(Collection $population): array
+    {
+        $sort = $population->sortByDesc(fn(Collection $chromosome, int $key) =>
+        $chromosome->last()['fitness_score'])->values();
+
+        return [$sort[0], $sort[1]];
     }
 
     private function splitLectureSlotsByCreditHour(): array
