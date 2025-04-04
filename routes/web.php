a6da0e94898441afec51d274a7cd3f69f94c52a8
Fajar Riansyah <fajary781@gmail.com>
2025-04-04 16:42:02 +0700

    feat: register views to web route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Resources\LectureResource;
+use App\Models\Course;
+use App\Models\Lecture;
+use App\Models\Lecturer;
+use App\Models\RoomClass;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,9 +17,16 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/lecturer', fn() => view('lecturer', ['lecturers' => Lecturer::all()]))->name('lecturer');
+    Route::get('/room-class', fn() => view('room-class', ['roomClasses' => RoomClass::all()]))->name('room-class');
+    Route::get('/course', fn() => view('course', ['courses' => Course::all()]))->name('course');
+    Route::get('/lecture', fn() => view('lecture', ['lectures' => LectureResource::collection(Lecture::all())]))->name('lecture');
+});
+
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
