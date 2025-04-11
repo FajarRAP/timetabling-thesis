0d7af3db75b133ba13f0708afb7861bf59dde6a4
Fajar Riansyah <fajary781@gmail.com>
2025-04-11 17:21:42 +0700

    feat: add some svgs

---
+++ resources/views/components/svgs/arrow-left.blade.php
@@ -0,0 +1,4 @@
+<svg {{ $attributes->merge(['class' => 'size-4 text-white rtl:rotate-180']) }} xmlns="http://www.w3.org/2000/svg"
+    fill="none" viewBox="0 0 6 10">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+</svg>
