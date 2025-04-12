3707e098c1e67e7a07917b37c6c17d8b10fd17b1
Fajar Riansyah <fajary781@gmail.com>
2025-04-12 08:09:42 +0700

    feat (navigation): implement collapse plugin

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -88,10 +88,25 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
+    <div x-show="open" x-collapse class="sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('timetable')" :active="request()->routeIs('timetable') || request()->routeIs('timetable-entry')">
+                {{ __('Timetable') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('lecture')" :active="request()->routeIs('lecture')">
+                {{ __('Lecture') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('lecturer')" :active="request()->routeIs('lecturer')">
+                {{ __('Lecturer') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('room-class')" :active="request()->routeIs('room-class')">
+                {{ __('Room Class') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('course')" :active="request()->routeIs('course')">
+                {{ __('Course') }}
             </x-responsive-nav-link>
         </div>
 
