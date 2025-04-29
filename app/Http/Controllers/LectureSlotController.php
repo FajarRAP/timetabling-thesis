1b8461538d828dc739aaf29e5f2dd70b121a318c
Fajar Riansyah <fajary781@gmail.com>
2025-04-29 13:20:26 +0700

    feat (lecture slot): update lecture slot retrieval to exclude constrained lecture slot

--- app/Http/Controllers/LectureSlotController.php
+++ app/Http/Controllers/LectureSlotController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Day;
 use App\Models\LectureSlot;
+use App\Models\LectureSlotConstraint;
 use Illuminate\Http\Request;
 
 class LectureSlotController extends Controller
@@ -13,8 +14,11 @@ class LectureSlotController extends Controller
      */
     public function __invoke(Request $request)
     {
+        $lectureSlots = LectureSlot::where('is_excluded', false)->get();
+
         return view('lecture-slot', [
-            'lecture_slots' => LectureSlot::all(),
+            'lecture_slots' => $lectureSlots,
+            'lecture_slot_constraints' => LectureSlotConstraint::all(),
             'days' => Day::all(),
         ]);
     }
--- database/migrations/2025_01_21_082840_create_lecture_slots_table.php
+++ database/migrations/2025_01_21_082840_create_lecture_slots_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->integer('day_id');
             $table->integer('room_class_id');
             $table->integer('time_slot_id');
+            $table->boolean('is_excluded')->default(false);
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
