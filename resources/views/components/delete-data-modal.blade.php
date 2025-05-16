be01aa924712dd9230b0fa35666d664b93e4373b
Fajar Riansyah <fajary781@gmail.com>
2025-05-16 17:35:36 +0700

    feat (delete data modal): add padding

--- resources/views/components/delete-data-modal.blade.php
+++ resources/views/components/delete-data-modal.blade.php
@@ -1,22 +1,24 @@
 @props(['name', 'action'])
 
 <x-modal :$name focusable>
-    <form method="POST" action="{{ $action }}" class="p-6">
+    <form method="POST" action="{{ $action }}">
         @csrf
         @method('DELETE')
 
-        <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Are you sure you want to delete this data?') }}
-        </h2>
+        <div class="p-6">
+            <h2 class="text-lg font-medium text-gray-900">
+                {{ __('Are you sure you want to delete this data?') }}
+            </h2>
 
-        <div class="mt-6 flex justify-end">
-            <x-secondary-button x-on:click="$dispatch('close')">
-                {{ __('Cancel') }}
-            </x-secondary-button>
+            <div class="mt-6 flex justify-end">
+                <x-secondary-button type="button" x-data x-on:click="$dispatch('close')">
+                    {{ __('Cancel') }}
+                </x-secondary-button>
 
-            <x-danger-button class="ms-3">
-                {{ __('Delete Data') }}
-            </x-danger-button>
+                <x-danger-button type="submit" class="ms-3">
+                    {{ __('Delete Data') }}
+                </x-danger-button>
+            </div>
         </div>
     </form>
 </x-modal>
