a692f77df4cbad99ca42459c9fd45060eaccddb8
Fajar Riansyah <fajary781@gmail.com>
2025-05-16 16:01:46 +0700

    feat (pagination): add pagination per page options

--- resources/views/components/pagination/pagination.blade.php
+++ resources/views/components/pagination/pagination.blade.php
@@ -1,13 +1,27 @@
 <nav class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4"
     aria-label="Table navigation">
-    <span class="text-sm font-normal text-gray-500 ">
-        {{ __('Showing') }}
-        @if ($paginator->firstItem())
-            <span class="font-semibold text-gray-900 ">{{ $paginator->firstItem() }}-{{ $paginator->lastItem() }}</span>
-        @endif
-        {{ __('of') }}
-        <span class="font-semibold text-gray-900 ">{{ $paginator->total() }}</span>
-    </span>
+    <div class="flex flex-col gap-2 text-gray-700">
+        <span class="text-sm font-medium">
+            {{ __('Showing') }}
+            @if ($paginator->firstItem())
+                <span class="font-bold text-gray-900">{{ $paginator->firstItem() }}-{{ $paginator->lastItem() }}</span>
+            @endif
+            {{ __('of') }}
+            <span class="font-bold text-gray-900">{{ $paginator->total() }}</span>
+        </span>
+        <span class="text-sm font-medium flex gap-3 items-center">
+            {{ __('Show Data Per Page') }}
+            <form action="{{ url()->current() }}" method="GET">
+                <x-select-input name="per_page" onchange="this.form.submit()">
+                    <x-slot name="options">
+                        <option value="10" {{ request()->query('per_page') == 10 ? 'selected' : '' }}>10</option>
+                        <option value="100"{{ request()->query('per_page') == 100 ? 'selected' : '' }}>100</option>
+                        <option value="1000"{{ request()->query('per_page') == 1000 ? 'selected' : '' }}>1000</option>
+                    </x-slot>
+                </x-select-input>
+            </form>
+        </span>
+    </div>
     <ul class="inline-flex items-stretch -space-x-px">
         @php
             $prevUrl = $paginator->onFirstPage() ? null : $paginator->previousPageUrl();
