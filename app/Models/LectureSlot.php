1b66b87c12fb98e187a19f65439377d14771c011
Fajar Riansyah <fajary781@gmail.com>
2025-05-16 14:14:45 +0700

    feat (lecture slot): make model fillable

--- app/Models/LectureSlot.php
+++ app/Models/LectureSlot.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LectureSlot extends Model
 {
+    protected $guarded = [];
+    
     public function day(): BelongsTo
     {
         return $this->belongsTo(Day::class);
