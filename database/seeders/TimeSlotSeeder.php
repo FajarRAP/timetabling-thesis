49583a68c05233ac5db6d8e91b0798f57809e4db
Fajar Riansyah <fajary781@gmail.com>
2025-05-16 18:37:04 +0700

    refactor (time slot seeder): divide into two types of data

--- database/seeders/TimeSlotSeeder.php
+++ database/seeders/TimeSlotSeeder.php
@@ -13,7 +13,7 @@ class TimeSlotSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('time_slots')->insert([
+        $violateSIMERU = [
             [
                 'time_slot' => '07.00-08.40',
                 'start_at' => '07:00:00',
@@ -140,6 +140,71 @@ class TimeSlotSeeder extends Seeder
                 'end_at' => '17:50:00',
                 'credit_hour' => 3,
             ],
-        ]);
+        ];
+
+        $unviolateSIMERU = [
+            [
+                'time_slot' => '07.00-08.40',
+                'start_at' => '07:00:00',
+                'end_at' => '08:40:00',
+                'credit_hour' => 2,
+            ],
+            [
+                'time_slot' => '08.45-10.25',
+                'start_at' => '08:45:00',
+                'end_at' => '10:25:00',
+                'credit_hour' => 2,
+            ],
+            [
+                'time_slot' => '10.30-12.10',
+                'start_at' => '10:30:00',
+                'end_at' => '12:10:00',
+                'credit_hour' => 2,
+            ],
+            [
+                'time_slot' => '12.30-14.10',
+                'start_at' => '12:30:00',
+                'end_at' => '14:10:00',
+                'credit_hour' => 2,
+            ],
+            [
+                'time_slot' => '14.15-16.05',
+                'start_at' => '14:15:00',
+                'end_at' => '16:05:00',
+                'credit_hour' => 2,
+            ],
+            [
+                'time_slot' => '16.10-17.50',
+                'start_at' => '16:10:00',
+                'end_at' => '17:50:00',
+                'credit_hour' => 2,
+            ],
+            [
+                'time_slot' => '07.00-09.35',
+                'start_at' => '07:00:00',
+                'end_at' => '09:35:00',
+                'credit_hour' => 3,
+            ],
+            [
+                'time_slot' => '09.35-12.10',
+                'start_at' => '09:35:00',
+                'end_at' => '12:10:00',
+                'credit_hour' => 3,
+            ],
+            [
+                'time_slot' => '12.30-15.05',
+                'start_at' => '12:30:00',
+                'end_at' => '15:05:00',
+                'credit_hour' => 3,
+            ],
+            [
+                'time_slot' => '15.15-17.50',
+                'start_at' => '15:15:00',
+                'end_at' => '17:50:00',
+                'credit_hour' => 3,
+            ],
+        ];
+
+        DB::table('time_slots')->insert($unviolateSIMERU);
     }
 }
