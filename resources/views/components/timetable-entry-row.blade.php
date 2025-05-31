0ee0601eeba356da3fb3a93422423efd2e60c333
Fajar Riansyah <fajary781@gmail.com>
2025-06-01 00:29:51 +0700

    fix (timetable entry row): update violation checking to use is_hard_violated property

--- resources/views/components/timetable-entry-row.blade.php
+++ resources/views/components/timetable-entry-row.blade.php
@@ -1,9 +1,9 @@
 @props(['index', 'timetable_entry'])
 
 <tr
-    {{ $attributes->class(['border-b border-gray-200', 'bg-red-600 hover:bg-red-100 hover:text-gray-700 text-white' => $timetable_entry['is_violated'], 'bg-white hover:bg-gray-50' => !$timetable_entry['is_violated']]) }}>
+    {{ $attributes->class(['border-b border-gray-200', 'bg-red-600 hover:bg-red-100 hover:text-gray-700 text-white' => $timetable_entry->is_hard_violated, 'bg-white hover:bg-gray-50' => !$timetable_entry->is_hard_violated]) }}>
     <th scope="row"
-        {{ $attributes->class(['px-6 py-4 font-medium whitespace-nowrap', 'text-gray-50' => $timetable_entry['is_violated'], 'text-gray-900' => !$timetable_entry['is_violated']]) }}>
+        {{ $attributes->class(['px-6 py-4 font-medium whitespace-nowrap', 'text-gray-50' => $timetable_entry->is_hard_violated, 'text-gray-900' => !$timetable_entry->is_hard_violated]) }}>
         {{ $index }}
     </th>
     <td class="px-6 py-4">
